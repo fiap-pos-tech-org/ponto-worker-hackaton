d01048372c4d5a90cb0075c14703380cc384c019
diego <oliveiraj.diego@gmail.com>
2024-03-24 00:46:33 -0300

    chore: remove spring boot context load

--- src/test/java/br/com/fiap/hackaton/clockregistryworker/PontoWorkerApplicationTests.java
+++ src/test/java/br/com/fiap/hackaton/clockregistryworker/PontoWorkerApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.fiap.hackaton.clockregistryworker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PontoWorkerApplicationTests {
 
     @Test
